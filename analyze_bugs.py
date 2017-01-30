db40d27faf4ea7c0fead9545aee69d95ba194942
Marco Castelluccio <mcastelluccio@mozilla.com>
2017-01-30 11:25:03 +0000

    Generate a separate CSV file for each channel

--- analyze_bugs.py
+++ analyze_bugs.py
@@ -115,8 +115,15 @@ if __name__ == '__main__':
             json.dump(analyzed_bugs, f)
 
 
-    row_keys = set()
     rows = []
+    row_keys = set()
+    rows_any = []
+    rows_per_channel = {
+      'release': [],
+      'beta': [],
+      'aurora': [],
+    }
+    row_per_channel_keys = set()
     for bug_id, info in analyzed_bugs.iteritems():
         info['bug_id'] = bug_id
 
@@ -136,10 +143,24 @@ if __name__ == '__main__':
         info['patch_reviewers'] = '^'.join(info['users']['reviewers'])
         del info['users']
 
+        # Expand arrays.
+        for key in info.keys():
+            if isinstance(info[key], list):
+                info[key] = '^'.join(list(set(info[key])))
+
         # Add uplift-related info.
-        for channel in ['aurora', 'beta', 'release']:
+        info_before = info.copy()
+        del info_before['channels']
+        for channel in ['release', 'beta', 'aurora']:
+            if (channel + '_uplift_info') in info_before:
+                del info_before[channel + '_uplift_info']
+
+        any_added = False
+        for channel in ['release', 'beta', 'aurora']:
             if (channel + '_uplift_info') not in info:
                 continue
+
+            row_per_channel = info_before.copy()
 
             info[channel + '_landing_delta'] = info[channel + '_uplift_info']['landing_delta']
             info[channel + '_response_delta'] = info[channel + '_uplift_info']['response_delta']
@@ -149,17 +170,44 @@ if __name__ == '__main__':
             info[channel + '_uplift_accepted'] = info[channel + '_uplift_info']['uplift_accepted']
             del info[channel + '_uplift_info']
 
-        # Expand arrays.
-        for key in info.keys():
-            if isinstance(info[key], list):
-                info[key] = '^'.join(list(set(info[key])))
+            row_per_channel['landing_delta'] = info[channel + '_landing_delta']
+            row_per_channel['response_delta'] = info[channel + '_response_delta']
+            row_per_channel['release_delta'] = info[channel + '_release_delta']
+            row_per_channel['uplift_comment_length'] = info[channel + '_uplift_comment_length']
+            row_per_channel['uplift_requestor'] = info[channel + '_uplift_requestor']
+            rows_per_channel[channel].append(row_per_channel)
 
-            row_keys.add(key)
+            if not any_added:
+                rows_any.append(row_per_channel)
+                any_added = True
+
+            row_per_channel_keys |= set(row_per_channel.keys())
+
+        # Add keys to the set of keys.
+        row_keys |= set(info.keys())
 
         rows.append(info)
 
+
+    row_keys.remove('bug_id')
+    row_keys = ['bug_id'] + sorted(list(row_keys))
+
     with open('independent_metrics/basic.csv', 'w') as output_file:
-        row_keys.remove('bug_id')
-        csv_writer = csv.DictWriter(output_file, ['bug_id'] + sorted(list(row_keys)))
+        csv_writer = csv.DictWriter(output_file, row_keys)
         csv_writer.writeheader()
         csv_writer.writerows(rows)
+
+
+    row_per_channel_keys.remove('bug_id')
+    row_per_channel_keys = ['bug_id'] + sorted(list(row_per_channel_keys))
+
+    with open('independent_metrics/basic_any.csv', 'w') as output_file:
+        csv_writer = csv.DictWriter(output_file, row_per_channel_keys)
+        csv_writer.writeheader()
+        csv_writer.writerows(rows_any)
+
+    for channel in ['release', 'beta', 'aurora']:
+        with open('independent_metrics/basic_' + channel + '.csv', 'w') as output_file:
+            csv_writer = csv.DictWriter(output_file, row_per_channel_keys)
+            csv_writer.writeheader()
+            csv_writer.writerows(rows_per_channel[channel])
