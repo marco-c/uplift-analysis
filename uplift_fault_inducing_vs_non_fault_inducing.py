46c11c7d52e53728820aa9d2216987cee6f14930
Marco Castelluccio <mcastelluccio@mozilla.com>
2017-03-22 20:19:36 +0100

    Remove Pocket bugs from the CSV files and add other bugs to replace some of them (to have the correct sample size)

--- uplift_fault_inducing_vs_non_fault_inducing.py
+++ uplift_fault_inducing_vs_non_fault_inducing.py
@@ -90,10 +90,10 @@ for bug in bugs:
 for channel in channels:
     print(channel)
 
-    print(len(uplifts_per_channel[channel]['fault']))
-    print(len(uplifts_per_channel[channel]['nonfault']))
-    print(sample_per_channel[channel]['fault'])
-    print(sample_per_channel[channel]['nonfault'])
+    print("Total fault-inducing uplifts: " + str(len(uplifts_per_channel[channel]['fault'])))
+    print("Total non-fault-inducing uplifts: " + str(len(uplifts_per_channel[channel]['nonfault'])))
+    print("Sample fault-inducing uplifts: " + str(sample_per_channel[channel]['fault']))
+    print("Sample non-fault-inducing uplifts: " + str(sample_per_channel[channel]['nonfault']))
 
     with open('uplift_fault_inducing_vs_non_fault_inducing_' + channel + '.csv', 'w') as output_file:
         csv_writer = csv.writer(output_file)
