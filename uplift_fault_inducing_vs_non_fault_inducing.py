756f5a6a49373a54695d9d03f8b0e7eec34403a5
Marco Castelluccio <mcastelluccio@mozilla.com>
2017-03-22 19:00:47 +0100

    Don't recalculate bug_inducing_bugs multiple times

--- uplift_fault_inducing_vs_non_fault_inducing.py
+++ uplift_fault_inducing_vs_non_fault_inducing.py
@@ -1,6 +1,7 @@
 import csv
 import re
 import random
+import json
 from collections import defaultdict
 from datetime import datetime
 from libmozdata import hgmozilla
@@ -8,33 +9,43 @@ from libmozdata.utils import as_utc
 import get_bugs
 import utils
 
-bug_inducing_bugs = defaultdict(set)
+try:
+    with open('all_bugs/bug_inducing_bugs.json', 'r') as f:
+        bug_inducing_bugs = json.load(f)
+except:
+    bug_inducing_bugs = defaultdict(set)
 
-bug_pattern1 = re.compile('[\t ]*bug[\t ]*([0-9]+)')
-bug_pattern2 = re.compile('b=([0-9]+)')
-bug_pattern3 = re.compile('bug=([0-9]+)')
+    bug_pattern1 = re.compile('[\t ]*bug[\t ]*([0-9]+)')
+    bug_pattern2 = re.compile('b=([0-9]+)')
+    bug_pattern3 = re.compile('bug=([0-9]+)')
 
-bug_inducing_commits = []
-with open('all_bugs/bug_inducing_commits.csv', 'rb') as csv_file:
-    csv_reader = csv.reader(csv_file)
-    next(csv_reader)
-    for row in csv_reader:
-        caused_bug_id = int(row[0])
-        bug_inducing_commits = [rev for rev in row[1].split('^') if rev != '']
-        for commit in bug_inducing_commits:
-            meta = hgmozilla.Revision.get_revision(channel='nightly', node=commit)
-            desc = meta['desc'].lower()
-            bug_id_match = re.search(bug_pattern1, desc)
-            if not bug_id_match:
-                bug_id_match = re.search(bug_pattern2, desc)
-            if not bug_id_match:
-                bug_id_match = re.search(bug_pattern3, desc)
+    bug_inducing_commits = []
+    with open('all_bugs/bug_inducing_commits.csv', 'rb') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        next(csv_reader)
+        for row in csv_reader:
+            caused_bug_id = str(row[0])
+            bug_inducing_commits = [rev for rev in row[1].split('^') if rev != '']
+            for commit in bug_inducing_commits:
+                meta = hgmozilla.Revision.get_revision(channel='nightly', node=commit)
+                desc = meta['desc'].lower()
+                bug_id_match = re.search(bug_pattern1, desc)
+                if not bug_id_match:
+                    bug_id_match = re.search(bug_pattern2, desc)
+                if not bug_id_match:
+                    bug_id_match = re.search(bug_pattern3, desc)
 
-            if bug_id_match:
-                bug_inducing_bugs[int(bug_id_match.group(1))].add(caused_bug_id)
-            else:
-                print(meta)
-                print('')
+                if bug_id_match:
+                    bug_inducing_bugs[str(bug_id_match.group(1))].add(caused_bug_id)
+                else:
+                    print(meta)
+                    print('')
+
+    for bug_id in bug_inducing_bugs.keys():
+        bug_inducing_bugs[bug_id] = list(bug_inducing_bugs[bug_id])
+
+    with open('all_bugs/bug_inducing_bugs.json', 'w') as f:
+        json.dump(bug_inducing_bugs, f)
 
 bugs = get_bugs.get_all()
 uplifts = utils.get_uplifts(bugs)
@@ -68,10 +79,10 @@ for bug in bugs:
         if uplift_date is None or uplift_date < as_utc(datetime(2014, 9, 1)) or uplift_date >= as_utc(datetime(2016, 8, 24)):
             continue
 
-        if int(bug['id']) in bug_inducing_bugs:
-            uplifts_per_channel[channel]['fault'].add(bug['id'])
+        if str(bug['id']) in bug_inducing_bugs:
+            uplifts_per_channel[channel]['fault'].add(str(bug['id']))
         else:
-            uplifts_per_channel[channel]['nonfault'].add(bug['id'])
+            uplifts_per_channel[channel]['nonfault'].add(str(bug['id']))
 
 for channel in channels:
     print(channel)
@@ -88,7 +99,7 @@ for channel in channels:
         fault_inducing = list(uplifts_per_channel[channel]['fault'])
         fault_inducing = random.sample(fault_inducing, sample_per_channel[channel]['fault'])
         for bug_id in fault_inducing:
-            csv_writer.writerow([bug_id, '^'.join([str(bid) for bid in list(bug_inducing_bugs[bug_id])]), '', ''])
+            csv_writer.writerow([bug_id, '^'.join(bug_inducing_bugs[bug_id]), '', ''])
 
         non_fault_inducing = list(uplifts_per_channel[channel]['nonfault'])
         non_fault_inducing = random.sample(non_fault_inducing, sample_per_channel[channel]['nonfault'])
