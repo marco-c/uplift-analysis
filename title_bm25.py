2a5147964bce010686183c543f3f717f9bfc5210
Le An <le.an@polymtl.ca>
2018-05-08 14:57:25 +0000

    Add bm25 script for title

--- title_bm25.py
+++ title_bm25.py
@@ -1,9 +1,16 @@
-import os, re
+import get_bugs
+import os, re, csv, subprocess
 from nltk.stem.snowball import SnowballStemmer
 from nltk.corpus import stopwords
 from HTMLParser import HTMLParser
 from qdr import Trainer, QueryDocumentRelevance
 import pandas as pd
+
+# Run shell command from a string
+def shellCommand(command_str):
+    cmd =subprocess.Popen(command_str.split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    cmd_out, cmd_err = cmd.communicate()
+    return cmd_out
 
 def removeCharacter(word):
     cleaned_word = ''
@@ -11,6 +18,7 @@ def removeCharacter(word):
     for l in word:
         if not l in unwanted_ch:
            cleaned_word += l
+    cleaned_word.replace('\n', ' ')
     return cleaned_word
 
 def removePunctuation(word):
@@ -23,54 +31,63 @@ def removePunctuation(word):
 
 def buildCorpus():
     # init result lists
-    corpus = list()
-    bug_ids = list()
+    corpus = dict()
     # init model obj
     stemmer = SnowballStemmer('english')
     stop_words = set(stopwords.words('english'))
     h = HTMLParser()
     # build corpus from bug reports
-    current_dir = os.getcwd()
-    bug_reports = os.listdir('analyzed_bugs')
-    os.chdir('analyzed_bugs')
-    for br in bug_reports:
-        with open(br) as f:
-            reader = f.read()
-            raw_title = re.findall(r'\<short_desc\>(.+?)\<\/short_desc\>', reader)[0]
-            stemmed_title = list()
-            for word in raw_title.split(' '):
-                if len(word):
-                    word = h.unescape(word)
-                    word = removePunctuation(word)
-                    stemmed_word = stemmer.stem(word)
-                    if (stemmed_word not in stop_words) and (len(stemmed_word) > 0):
-                        stemmed_title.append(stemmed_word)
-            corpus.append(stemmed_title)
-            bug_ids.append(br[:-4])
-    os.chdir(current_dir)
-    return corpus, bug_ids
+    all_bugs = get_bugs.get_all()
+    for bug_item in all_bugs:
+        raw_title = bug_item['summary']
+        stemmed_title = list()
+        for word in raw_title.split(' '):
+            if len(word):
+                word = h.unescape(word)
+                word = removePunctuation(word)
+                stemmed_word = stemmer.stem(word)
+                if (stemmed_word not in stop_words) and (len(stemmed_word) > 0):
+                    stemmed_title.append(stemmed_word.encode('utf8'))
+        corpus[bug_item['id']] = stemmed_title        
+    return corpus
+
+def loadBugIDs(filename, uplift_bugs):
+    with open('independent_metrics/' + filename) as f:
+        reader = csv.reader(f)
+        next(reader, None)
+        for row in reader:
+            uplift_bugs.add(int(row[0]))
+    return uplift_bugs
 
 if __name__ == "__main__":
-    corpus, bug_ids = buildCorpus()
-
+    print 'Building corpus ...'
+    corpus = buildCorpus()
+    print 'Loading uplift bugs ...'
+    uplift_bugs = set()
+    uplift_bugs = loadBugIDs('basic_aurora.csv', uplift_bugs)
+    uplift_bugs = loadBugIDs('basic_beta.csv', uplift_bugs)
+    uplift_bugs = loadBugIDs('basic_release.csv', uplift_bugs)
+    print 'Training model ...'
     model = Trainer()
-    model.train(corpus)
+    model.train(corpus.values())
     model.serialize_to_file('trained_model.gz')
-
     scorer = QueryDocumentRelevance.load_from_file('trained_model.gz')
     result_list = list()
-    for i in range(len(corpus)-1):
-        for j in range(i+1, len(corpus)):
-            #print i, j
-            relevance_scores = scorer.score(corpus[i], corpus[j])
-            if relevance_scores['bm25'] > 0:
-#                print bug_ids[i], bug_ids[j], '%.2f' %relevance_scores['bm25']
-                result_list.append([bug_ids[i], bug_ids[j], relevance_scores['bm25']])
-    
+    analyzed_pairs = set()
+    for all_id in corpus:
+        for uplift_id in uplift_bugs:
+            if not (all_id == uplift_id):
+                id_pair = sorted([all_id, uplift_id])
+                pair_str = '%d^%d' %(id_pair[0],id_pair[1])
+                if not pair_str in analyzed_pairs:
+                    analyzed_pairs.add(pair_str)
+                    relevance_scores = scorer.score(corpus[id_pair[0]], corpus[id_pair[1]])
+                    if relevance_scores['bm25'] > 0:
+    #                    print bug_ids[i], bug_ids[j], '%.2f' %relevance_scores['bm25']
+                        result_list.append([corpus[id_pair[0]], corpus[id_pair[1]], relevance_scores['bm25']])
+    shellCommand('sudo sysctl -w vm.drop_caches=3')
+    print 'Outputing results ...'
     df = pd.DataFrame(result_list, columns=['bug1', 'bug2', 'bm25'])
-    sorted_df = df.sort_values(by='bm25', ascending=False)
-    print sorted_df
+    sorted_df = df.sort_values(by='bm25', ascending=False).round({bm25:2})
+#    print sorted_df
     sorted_df.to_csv('title_relevance.csv', index=False)
-
-
-
