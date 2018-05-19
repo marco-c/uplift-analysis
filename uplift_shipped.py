32722a4c8ee67a7fe857ae4e166870064810e719
Marco Castelluccio <mcastelluccio@mozilla.com>
2018-05-19 18:01:05 +0200

    Also add uplift ID in the CSV

--- uplift_shipped.py
+++ uplift_shipped.py
@@ -125,8 +125,8 @@ for channel in ['aurora', 'beta', 'release']:
             csv_writer.writerow([uplift['id'], '^'.join([str(i) for i in regression_ids])])
 
     with open('manual_classification/regressions_shipped_to_users_{}.csv'.format(channel), 'w') as output_file:
-        csv_writer = csv.writer(output_file, ['regression_id', 'reproducible'])
-        csv_writer.writerow(['regression_id', 'reproducible'])
+        csv_writer = csv.writer(output_file, ['uplift_id', 'regression_id', 'reproducible'])
+        csv_writer.writerow(['uplift_id', 'regression_id', 'reproducible'])
 
         for uplift in uplifts:
             if str(uplift['id']) not in regressions_shipped_to_users:
@@ -141,4 +141,4 @@ for channel in ['aurora', 'beta', 'release']:
                 continue
 
             for regression_id in regression_ids:
-                csv_writer.writerow([regression_id, ''])
+                csv_writer.writerow([uplift['id'], regression_id, ''])
