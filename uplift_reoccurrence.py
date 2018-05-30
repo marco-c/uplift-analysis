68f974e4f907f712df58bb734a38355b7beb27d8
Marco Castelluccio <mcastelluccio@mozilla.com>
2018-05-30 12:09:34 +0200

    Generate CSV files

--- uplift_reoccurrence.py
+++ uplift_reoccurrence.py
@@ -19,10 +19,15 @@ cloned_bug_map = utils.get_cloned_map(bugs)
 
 uplift_num = defaultdict(int)
 reopened = defaultdict(int)
+reopened_bugs = set()
 cloned = defaultdict(int)
+cloned_bugs = set()
 additionally_uplifted = defaultdict(int)
+additionally_uplifted_bugs = set()
 bm25_dupes_opened_after = defaultdict(int)
+bm25_dupes_opened_after_bugs = set()
 bm25_dupes_resolved_after = defaultdict(int)
+bm25_dupes_resolved_after_bugs = set()
 
 
 def get_bug_from_id(bug_id):
@@ -94,51 +99,58 @@ def is_reopened(bug, uplift_date):
         history_date = get_date_ymd(history['when'])
         for change in history['changes']:
             if change['field_name'] == 'status' and change['added'] == 'REOPENED' and history_date > uplift_date:
+                reopened_bugs.add(bug['id'])
                 return True
 
     return False
 
 
 def is_cloned(cloned_bug_map, bug, uplift_date):
+    ret = False
     if bug['id'] in cloned_bug_map:
         for cloned_bug in cloned_bug_map[bug['id']]:
             creation_date = get_date_ymd(cloned_bug['creation_time'])
             if creation_date > uplift_date:
-                return True
+                cloned_bugs.add((bug['id'], cloned_bug['id']))
+                ret = True
 
-    return False
+    return ret
 
 
-def is_additional_uplifts(uplift_dates):
+def is_additional_uplifts(bug, uplift_dates):
     if len(uplift_dates) == 1:
         return False
 
     for uplift_date in uplift_dates[1:]:
         if uplift_date > (uplift_dates[0] + timedelta(3)):
+            additionally_uplifted_bugs.add(bug['id'])
             return True
 
 
 def is_bm25_opened_after(bug, uplift_date):
+    ret = False
     if bug['id'] in bm25_data:
         dupes = bm25_data[bug['id']]
         for dupe in dupes:
             data = get_bug_from_id(dupe)
             if get_date_ymd(data['creation_time']) > uplift_date:
-                return True
+                bm25_dupes_opened_after_bugs.add((bug['id'], dupe))
+                ret = True
 
-    return False
+    return ret
 
 
 def is_bm25_resolved_after(bug, uplift_date):
+    ret = False
     if bug['id'] in bm25_data:
         dupes = bm25_data[bug['id']]
         for dupe in dupes:
             data = get_bug_from_id(dupe)
             if get_date_ymd(data['cf_last_resolved']) > uplift_date:
-                return True
+                bm25_dupes_resolved_after_bugs.add((bug['id'], dupe))
+                ret = True
 
-    return False
-
+    return ret
 
 
 for uplift in uplifts:
@@ -166,7 +178,7 @@ for uplift in uplifts:
             cloned[channel] += 1
             continue
 
-        if is_additional_uplifts(uplift_dates):
+        if is_additional_uplifts(uplift, uplift_dates):
             additionally_uplifted[channel] += 1
             continue
 
@@ -188,3 +200,38 @@ for channel in ['release', 'beta', 'aurora']:
     print('{} uplift bugs were found as duplicate via bm25 data, resolved after the uplift'.format(bm25_dupes_resolved_after[channel]))
     print('\n')
     print('\n')
+
+with open('manual_classification/reoccurrence/reopened.csv', 'w') as f:
+    csv_writer = csv.writer(f)
+    csv_writer.writerow(['uplift_id', 'why'])
+
+    for bug in reopened_bugs:
+        csv_writer.writerow([bug, ''])
+
+with open('manual_classification/reoccurrence/cloned.csv', 'w') as f:
+    csv_writer = csv.writer(f)
+    csv_writer.writerow(['uplift_id', 'cloned_id', 'why'])
+
+    for bug1, bug2 in cloned_bugs:
+        csv_writer.writerow([bug1, bug2, ''])
+
+with open('manual_classification/reoccurrence/additionally_uplifted.csv', 'w') as f:
+    csv_writer = csv.writer(f)
+    csv_writer.writerow(['uplift_id', 'why'])
+
+    for bug in additionally_uplifted_bugs:
+        csv_writer.writerow([bug, ''])
+
+with open('manual_classification/reoccurrence/bm25_opened_after.csv', 'w') as f:
+    csv_writer = csv.writer(f)
+    csv_writer.writerow(['uplift_id', 'dupe_id', 'why'])
+
+    for bug1, bug2 in bm25_dupes_opened_after_bugs:
+        csv_writer.writerow([bug1, bug2, ''])
+
+with open('manual_classification/reoccurrence/bm25_resolved_after.csv', 'w') as f:
+    csv_writer = csv.writer(f)
+    csv_writer.writerow(['uplift_id', 'dupe_id', 'why'])
+
+    for bug1, bug2 in bm25_dupes_resolved_after_bugs:
+        csv_writer.writerow([bug1, bug2, ''])
